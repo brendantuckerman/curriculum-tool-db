d85947932368cb61cb4665ad117bb9c6732d4b15
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2023-01-22 17:20:53 +1100

    Added seed for admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+## Start Seeds ##
+
+## User Seeds ##
+
+User.destroy_all
+
+u1 = User.create!(
+    name: "admin",
+    email: "admin@ctool.com",
+    password_digest: "chicken",
+    admin: true
+
+)
+
+puts "####### Creating Users ########"
+puts "I have created #{User.count} users."
+puts "The names of the users I have created are: #{User.pluck(:name).join(', ')}."
+
+## Year Seeds ##
+
