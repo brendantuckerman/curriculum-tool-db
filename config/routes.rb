86b777983a9f26955449d34297e254924577ff8c
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2023-02-16 19:01:29 +1100

    Added routes for all models

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  
+
   get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
+  
+  resources :users, :achievement_standards, :capabilities, :class_groups, :content_descriptors, :learning_areas, :modes, :priorities, :strands, :students, :subjects, :substrands, :subthreads, :tasks, :year_descriptions, :years
+
+
 end
